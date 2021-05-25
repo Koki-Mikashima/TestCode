6a6a2ad2f79d9c286e22fa3cc3be059584c21eb6
Koki-Mikashima <kokiumichan@gmail.com>
2021-05-25 17:03:22 +0900

    ウィンドウテキスト変更

--- Project2_2/main.cpp
+++ Project2_2/main.cpp
@@ -4,6 +4,7 @@
 
 bool SysInit(void)
 {
+	SetWindowText("2016033_ŽOƒ–“‡Ÿä‹G");
 	SetGraphMode(640, 480, 32);
 	ChangeWindowMode(true);
 	if (DxLib_Init() == -1)
@@ -24,9 +25,7 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 	while (ProcessMessage() == 0 && (CheckHitKey(KEY_INPUT_ESCAPE)) == 0)
 	{
 		ClearDrawScreen();
-		DrawBox(0, 0, 100, 100, 0xffff00, true);
 
-		DrawBox(0, 0,100,100, 0xffffff, true);
 		ScreenFlip();
 		//OutputDebugStringA();
 	}
