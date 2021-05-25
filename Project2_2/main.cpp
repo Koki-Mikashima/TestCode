6feda633bfcbc5fdbc519a0552751c2dc3cb009a
Koki-Mikashima <kokiumichan@gmail.com>
2021-05-25 16:53:46 +0900

    箱を描画

--- Project2_2/main.cpp
+++ Project2_2/main.cpp
@@ -24,6 +24,7 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 	while (ProcessMessage() == 0 && (CheckHitKey(KEY_INPUT_ESCAPE)) == 0)
 	{
 		ClearDrawScreen();
+		DrawBox(0, 0,100,100, 0xffffff, true);
 		ScreenFlip();
 		//OutputDebugStringA();
 	}
